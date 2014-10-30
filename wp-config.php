e7b36cfd76dab94ef7490d652cc2bdb30de10494
Troy Toman <troy@tomanator.com>
2014-10-30 17:43:45 -0500

    fixup another wp-config.php typo

--- wp-config.php
+++ wp-config.php
@@ -48,7 +48,7 @@ define('DB_COLLATE', '');
 define('AUTH_KEY',         getenv('WP_AUTH_KEY'));
 define('SECURE_AUTH_KEY',  getenv('WP_SECURE_AUTH_KEY'));
 define('LOGGED_IN_KEY',    getenv('WP_LOGGED_IN_KEY'));
-define('NONCE_KEY',        getenv('WP_NONCE_KEY');
+define('NONCE_KEY',        getenv('WP_NONCE_KEY'));
 define('AUTH_SALT',        getenv('WP_AUTH_SALT'));
 define('SECURE_AUTH_SALT', getenv('WP_SECURE_AUTH_SALT'));
 define('LOGGED_IN_SALT',   getenv('WP_LOGGED_IN_SALT'));
